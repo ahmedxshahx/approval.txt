999f7e6a6216e19ccf3eba9c996a60652021ce1b
ahmedxshahx <106920072+ahmedxshahx@users.noreply.github.com>
2022-06-27 11:39:20 +0500

    devil1.py

--- devil1.py
+++ devil1.py
@@ -45,8 +45,50 @@ logo = """
 
 """
 
-#Main Board Jaha Sy Script Start Hota 
+ 
 def main():
+ def Token_token():
+ os.system('clear')
+	print logo=
+	
+	print("")
+	try:
+		tok = open('/sdcard/Download/.ahm.', 'r').read()
+		dec = base64.b64decode(tok)
+		if len(dec) < 50:
+		    not_reg()
+		r = requests.get("pastebin.com/raw/Ah0273Lis").text
+		if dec in r:
+			main() #add your main menu when payment sucess
+		else:
+		    print('\t  \033[1;91m YOUR  Token_token  IS NOT  APPROVED :(').center(50)
+		    print("")
+		    print('\033[1;93m Your Token_token : \033[1;92m'+dec+'\n\n\n')
+		    raw_input("  \t  \033[1;93m Press enter to check again ")
+		    not_reg()
+	except (IOError):
+	    not_reg()
+	except requests.exceptions.ConnectionError:
+	    print('\n\n\nTurn on mobile data OR wifi to continue\n\n')
+	    sys.exit()
+
+	
+
+def not_reg():
+    print('\n\n\nVerify your Token_token\n\n\n')
+    string_Token_token = 'abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    v_Token_token = ''.join((random.choice(string_Token_token)) for x in range(50))
+    v_Token_token_save = open('/sdcard/Download/Approval.txt', 'w')
+    v_Token_token_save.write(base64.b64encode(v_Token_token))
+    v_Token_token_save.close()
+    print('\033[1;93m Your Token_token : \033[1;92m' + v_Token_token)
+    print("")
+    print("")
+    raw_input("  \t  \033[1;93m Press enter to check again ")
+    os.system('xdg-open https://wa.me/+9231835824') #whatsapp number here
+    Token_token()
+
+    
     os.system('clear')
     print logo
     print ''
